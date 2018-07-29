3d50b98a5cea3a17af52023f297fcb3450d5ac9e
prattita <prattita@hawk.iit.edu>
2018-07-28 20:55:05 -0500

    Add a door object and a door rotation component

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Copyright Paolo Ratti Tamayo
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/* Rotate the door at the start of the game*/
+	//Find the owning actor
+	AActor* Owner = GetOwner();
+
+	//Create a rotator. It is an object that stores rotation information in degrees.
+	FRotator NewRotation = FRotator(0.0f, 30.0f, 0.0f);
+
+	Owner->SetActorRotation(NewRotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
